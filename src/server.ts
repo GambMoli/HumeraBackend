6f271fd7089959b6ac59e512c9615525babe4109
GambMoli <g.molina@nelmbo.com.co>
2026-02-09 10:11:55 -0500

    "Feat"

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
-import app from './app.ts';
-import dotenv from 'dotenv';
+import app from './app.js';
 
-dotenv.config();
+const PORT = Number(process.env.PORT) || 8080;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
